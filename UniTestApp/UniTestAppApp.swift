968174a6980c7f719d13364ea32486f118b6e6d0
Jelson J <50907905+jelsonjay@users.noreply.github.com>
2023-02-16 09:35:35 +0000

    Initial Commit

---
+++ UniTestApp/UniTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniTestAppApp.swift
+//  UniTestApp
+//
+//  Created by Jelson J on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct UniTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
